9405f468ad283dc20cec55cb908af5f423fa003b
Akhilesh <37409441+AKHILESH1705@users.noreply.github.com>
2019-03-08 12:19:03 +0530

    pattern2.py
    decending order

--- pattern.py
+++ pattern.py
@@ -1,5 +1,5 @@
 num= int(input("enter number of row : "))
-for i in range(1,num+1):
-    for j in range(1,i+1):
+for i in range(num,0,-1):
+    for j in range(0,i):
         print("*",end=" ")
     print() 
